5264cb41414e29b50a73601e4aa6edc21d4a9952
Nguyen Quan <Nguyen Quan>
2017-01-25 14:45:42 +0700

    fix firebase auth

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FirebaseAuth, FirebaseAuthState, AngularFire } from "angularfire2";
+import { FirebaseAuthState, AngularFire } from "angularfire2";
 import { Router } from "@angular/router";
 
 import { BehaviorSubject, Subject, Observable } from "rxjs/Rx";
@@ -7,10 +7,13 @@ import { BehaviorSubject, Subject, Observable } from "rxjs/Rx";
 @Injectable()
 export class AuthService {
 
-  constructor(private auth: FirebaseAuth,
-              private router: Router,
+  auth: any;
+
+  constructor(private router: Router,
               private af: AngularFire
-  ) { }
+  ) {
+    this.auth = af.auth;
+  }
 
   loginEmail(email, password): Observable<FirebaseAuthState> {
     return this.fromFirebaseAuthPromise(this.auth.login({email, password}));
